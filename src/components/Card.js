01b7d5e409bcfec4b1a7545081a1a2d5936e093e
Siddharth Pal <originalsidd@gmail.com>
2021-05-04 15:24:51 +0530

    Updated Design

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
 	return (
 		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robots' src={`https://robohash.org/${id}?size=260x260`} />
+			<img alt='robots' src={`https://robohash.org/${id}?size=150x150`} />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
